ffe6231bfd9c0afca0c0735d2ba855e897f1d4d5
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-04-16 08:39:53 +0300

    labs№10

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -1,8 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function BookCard(props){
-    const [localCount, setLocalCount] = useState(0);
+    const [localCount, setLocalCount] = useState(() =>{
+      const saved = localStorage.getItem('count_1${props.title}');
+      return saved ? parseInt(saved) : 0;
+    });
+  useEffect(() =>{
+    localStorage.setItem('count_${props.title}', localCount);}, [localCount, props.title]);
   
     const handeBuyClick = () => {
         setLocalCount(localCount + 1);
@@ -17,6 +22,13 @@ function BookCard(props){
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.price} грн</p>
+            
+            <div className="mb-3">
+              <span className="badge bg-info text-dark">
+                У кошику:{localCount} шт.
+              </span>
+            </div>
+
             <button onClick={handeBuyClick}>Купити</button>
           </div>
         </div>
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react"; 
 import BookCard from "./BookCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Main({ items, onBuy }) { 
+  const [isLoading, setIsLoading] = useState(true);
+  const [timeLeft, setTimeLeft] = useState(15);
+
+  useEffect(() =>{
+    const timer = setTimeout(() =>{
+      setIsLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() =>{
+    if (timeLeft <= 0) return;
+    const interval = setInterval(() =>{
+      setTimeLeft(prev => prev - 1 );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeLeft]);
+
+  if (isLoading) {
+    return (
+      <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: '50vh' }}>
+        <img src="https://i.gifer.com/ZKZg.gif" alt="Loading..." width="100" />
+        <h4 className="mt-3 text-secondary">Завантаження каталогу...</h4>
+      </div>
+    );
+  }
   return (
-<main className="mainContent">
+<main className="mainContent conteiner py-4">
+  {timeLeft > 0 && (
+    <div className=" alert alert-warning text-center shadow-sm border-0 mb-4" style={{borderRadius:'15px'}}>
+      <span className="fs-5">Акція завершується через : <strong>{timeLeft}сек.</strong></span>
+    </div>
+  )}
       <div className="text-center py-5 mb-4 bg-light rounded-3 shadow-sm">
         <h1 className="display-5 fw-bold">Ласкаво просимо на наш сайт</h1>
         <p className="lead text-muted mx-auto" style={{ maxWidth: '600px' }}>
