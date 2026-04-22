d888378c1d530af0ce87800b568ec1ddba4ef2b0
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-04-22 19:36:00 +0300

    rename

--- src/Components/CatalogPage.jsx
+++ src/Components/CatalogPage.jsx
@@ -10,7 +10,7 @@ const CatalogPage = ({ items, onBuy }) => {
     <div key={book.id} className="card col-md-4 mb-3">
      <div className="card-body">
      <h5 className="card-title">{book.title}</h5>
-     <p className="card-text">{book.price} грн</p>
+     <p className="card-text">{book.price} UAH</p>
      <Link to={`/book/${book.id}`} className="btn btn-secondary me-2">More</Link>
 <button onClick={() => onBuy(book.price)} className="btn btn-primary">Buy</button>
     </div>
